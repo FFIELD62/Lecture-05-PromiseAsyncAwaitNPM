2d24fe77ddfb996dd41c640ea265b71ce9c608a8
FFIELD62 <120073007+FFIELD62@users.noreply.github.com>
2023-01-19 08:39:52 +0700

    Update asynccatch.js

--- asynccatch.js
+++ asynccatch.js
@@ -1,7 +1,7 @@
 function yayOrNay() {
     return new Promise ((resolve, reject) => {
     const val = Math.round(Math.random() * 1);
-    // 0 or 1 , at random
+    // 0  or 1 , at random
     val ? resolve("Lucky!") : reject("Nope!");
     });
 }
